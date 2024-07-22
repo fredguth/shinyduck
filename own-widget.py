eb85f8eaa493dc6442d17b7c323e72a0a1947b34
Fred Guth <fredguth@fredguth.com>
2024-07-22 16:32:30 -0300

    feat appshell

--- own-widget.py
+++ own-widget.py
@@ -8,16 +8,7 @@ import shinyswatch
 
 @module.ui
 def toggleButtons_ui() -> ui.TagChild:
-    return ui.panel_absolute(
-        ui.panel_well(
-            ui.output_ui("menu"),
-        ),
-        draggable=False,
-        width="100%",
-        height="100%",
-        top="0%",
-        left="0%"
-    )
+    return ui.output_ui("menu")
     
 
 @module.server
@@ -27,7 +18,7 @@ def toggleButtons_server(input: Inputs, output: Outputs, session: Session, choic
     def menu():
         buttons = []
         for choice in choices:
-            klass = "btn-primary" if selected() == choice else "btn-scondary"
+            klass = "btn-secondary" if selected() == choice else "btn-outline-secondary"
             buttons.append(ui.input_action_button(f"menubtn_{choices.index(choice)}", f"{choice}", class_=klass))
         return ui.div(*buttons, class_="d-grid gap-2")
 
@@ -51,10 +42,11 @@ def tableShell_ui() -> ui.TagChild:
 def tableShell_server(input, output, session, title:str=""):
     @render.ui
     def tableShell():
+        t = f"Tabela: {title}"
         panels = ["Baixar", "Explorar", None, None, "Documentação", "Linhagem", "Qualidade", "Ficha Técnica", None, None, None, None, None]
         children = [ui.nav_panel(c, f"Panel {c} content") if c else ui.nav_spacer() for c in panels]
-        sb = ui.sidebar("tbl_sb", open="closed", title=title, position="right")
-        return ui.navset_card_underline(id="tbl_card", selected="Explorar", sidebar=sb, title=title, *children)
+        sb = ui.sidebar("tbl_sb", open="closed", title=t, position="right")
+        return ui.navset_card_underline(id="tbl_card", selected="Explorar", sidebar=sb, title=t, *children)
 
 
 
@@ -62,82 +54,31 @@ def tableShell_server(input, output, session, title:str=""):
 # =============================================================================
 # App that uses module
 # =============================================================================
-app_ui = ui.page_fluid(
+
+
+panels = [ui.accordion_panel(p, p) for p in ["Entradas", "Saídas", "Reúsos", "Discussão"]]
+tbl_panel = ui.accordion_panel("Tabelas", toggleButtons_ui("main_sidebar"))
+accordion = ui.accordion(tbl_panel, *panels)
+
+app_ui = ui.page_sidebar(        
+    ui.sidebar(toggleButtons_ui("menu1"), id="main_sb", open="open", title="Base: SIOPS", position="left"),
     tableShell_ui(id="shell"),
-    # toggleButtons_ui("menu1"),
-    # ui.output_ui("shell"),
-    
-    # theme=shinyswatch.theme.sandstone
+    # ui.tags.style(".bslib-sidebar-layout > .main { padding: 10px; padding-top: 0px;}"),
 )
 
 
 def server(input, output, session):
-    tableShell_server(id="shell", title="Entes")
-    # choices = ["Entes", "Receitas", "Despesas"]
-    # selected = reactive.value(choices[0])
-
-    # @render.ui
-    # def shell():    
-    #     tableShell_ui(id="shell", title="Entes"),
-    #     ui.br()
-        
-        
     
-    # toggleButtons_server("menu1", choices, selected)
+    choices = ["Entes", "Receitas", "Despesas"]
+    selected = reactive.value(choices[0])
+    toggleButtons_server("menu1", choices, selected)
+    tableShell_server(id="shell", title="Entes")
+
+    
+    
     
     
 
 
 app = App(app_ui, server)
-
-
-
-
-
-# # ============================================================
-# # Counter module
-# # ============================================================
-# @module.ui
-# def counter_ui(label: str = "Increment counter") -> ui.TagChild:
-#     return ui.card(
-#         ui.h2("This is " + label),
-#         ui.input_action_button(id="button", label=label),
-#         ui.output_code(id="out"),
-#     )
-
-
-# @module.server
-# def counter_server(input, output, session, starting_value: int = 0):
-#     count: reactive.value[int] = reactive.value(starting_value)
-
-#     @reactive.effect
-#     @reactive.event(input.button)
-#     def _():
-#         count.set(count() + 1)
-
-#     @render.code
-#     def out() -> str:
-#         return f"Click count is {count()}"
-
-
-# # =============================================================================
-# # App that uses module
-# # =============================================================================
-# app_ui = ui.page_fluid(
-#     counter_ui("counter1", "Counter 1"),
-#     counter_ui("counter2", "Counter 2"),
-# )
-
-
-# def server(input, output, session):
-#     counter_server("counter1")
-#     counter_server("counter2")
-
-
-# app = App(app_ui, server)
-
-
-
-
-
 
